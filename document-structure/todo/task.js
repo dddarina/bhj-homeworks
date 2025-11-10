dd2e872af0c89ba3b6c2e670a409fb2fbfe34d04
darinadd <diyardarina212005@gmail.com>
2025-11-10 21:19:32 +0500

    Исправлено пятое задание

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,38 +1,45 @@
 const tasksList = document.getElementById('tasks__list');
 const taskInput = document.getElementById('task__input');
+const tasksForm = document.getElementById('tasks__form');
+const addButton = document.querySelector('.tasks__add');
 
 function createTask(text) {
-    const task = document.createElement('div');
-    task.className = 'task';
-    
-    const taskTitle = document.createElement('div');
-    taskTitle.className = 'task__title';
-    taskTitle.textContent = text;
-    
-    const taskRemove = document.createElement('a');
-    taskRemove.href = '#';
-    taskRemove.className = 'task__remove';
-    taskRemove.innerHTML = '&times;';
-    
-    taskRemove.addEventListener('click', function(event) {
+    tasksList.insertAdjacentHTML('afterbegin', `
+        <div class="task">
+            <div class="task__title">
+                ${text}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>
+    `);
+
+    const newTask = tasksList.querySelector('.task');
+    const removeBtn = newTask.querySelector('.task__remove');
+
+    removeBtn.addEventListener('click', function (event) {
         event.preventDefault();
-        task.remove();
+        newTask.remove();
     });
-    
-    task.appendChild(taskTitle);
-    task.appendChild(taskRemove);
-    
-    return task;
 }
 
-taskInput.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter' && taskInput.value.trim() !== '') {
-        const newTask = createTask(taskInput.value.trim());
-        
-        tasksList.appendChild(newTask);
-        
+tasksForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        createTask(taskText);
         taskInput.value = '';
-        
-        event.preventDefault();
+    }
+});
+
+addButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        createTask(taskText);
+        taskInput.value = '';
     }
 });
